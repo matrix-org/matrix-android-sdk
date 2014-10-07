284a9f2d9ab07f95e396f4c08f4852e9651df008
Kegan Dougal <kegan@matrix.org>
2014-10-07 10:55:17 +0100

    Configure test environment.

--- sdk/src/androidTest/java/org/matrix/androidsdk/ApplicationTest.java
+++ sdk/src/androidTest/java/org/matrix/androidsdk/ApplicationTest.java
@@ -3,11 +3,17 @@ package org.matrix.androidsdk;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import junit.framework.Assert;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testTrue(){
+        Assert.assertTrue(true);
     }
 }
