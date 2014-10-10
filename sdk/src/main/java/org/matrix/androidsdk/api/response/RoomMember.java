7d87d88fa0c508dec83805f664f50b433e8c8cf4
joachimre <joachim@matrix.org>
2014-10-10 15:31:54 +0200

    Added license information

--- sdk/src/main/java/org/matrix/androidsdk/api/response/RoomMember.java
+++ sdk/src/main/java/org/matrix/androidsdk/api/response/RoomMember.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2014 OpenMarket Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.matrix.androidsdk.api.response;
 
 /**
- * Created by JOACHIMR on 03/10/2014.
+ * Class representing a room member: a user with membership information.
  */
 public class RoomMember extends User {
     public String membership;
--- sdk/src/main/java/org/matrix/androidsdk/data/Room.java
+++ sdk/src/main/java/org/matrix/androidsdk/data/Room.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 OpenMarket Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.matrix.androidsdk.data;
 
 import org.matrix.androidsdk.api.response.Event;
@@ -7,7 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by JOACHIMR on 03/10/2014.
+ * Representation of a room, with messages, members, and state.
  */
 public class Room {
 
