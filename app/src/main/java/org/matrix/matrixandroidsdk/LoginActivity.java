0760f005935666aa0cbdc1dfca128c665f8288f9
Kegan Dougal <kegan@matrix.org>
2014-10-10 12:31:20 +0100

    Add login and room activities.

---
+++ app/src/main/java/org/matrix/matrixandroidsdk/LoginActivity.java
@@ -0,0 +1,14 @@
+package org.matrix.matrixandroidsdk;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
