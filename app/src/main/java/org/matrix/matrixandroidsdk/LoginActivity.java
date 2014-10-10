80f94a5850048294c6754381ff6951897964c71e
Kegan Dougal <kegan@matrix.org>
2014-10-10 16:42:09 +0100

    Add stub fragments. Add LoginStorage impl to store login credentials via SharedPreferences.

--- app/src/main/java/org/matrix/matrixandroidsdk/LoginActivity.java
+++ app/src/main/java/org/matrix/matrixandroidsdk/LoginActivity.java
@@ -1,9 +1,14 @@
 package org.matrix.matrixandroidsdk;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import org.matrix.matrixandroidsdk.store.LoginStorage;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -13,18 +18,31 @@ public class LoginActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if (hasCredentials()) {
+            goToHomepage();
+            finish();
+        }
+
         findViewById(R.id.button_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onLoginClick();
+                String username = ((EditText)findViewById(R.id.editText_username)).getText().toString();
+                String password = ((EditText)findViewById(R.id.editText_password)).getText().toString();
+                onLoginClick(username, password);
             }
         });
     }
 
-    private void onLoginClick() {
-        // TODO submit credentials
+    private void onLoginClick(String username, String password) {
+        // TODO submit credentials then save via LoginStorage.saveCredentials(Credentials)
         goToHomepage();
     }
+
+    private boolean hasCredentials() {
+        LoginStorage ls = new LoginStorage(this);
+        return ls.hasCredentials();
+    }
+
 
     private void goToHomepage() {
         startActivity(new Intent(this, HomeActivity.class));
