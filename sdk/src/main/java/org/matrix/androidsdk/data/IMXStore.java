4b95d7bd682d0fe9d2d1d0efcfb0483c165818bf
Kegan Dougal <kegan@matrix.org>
2014-10-14 13:32:43 +0100

    Split storage logic out from MXData.
    Split into MXMemoryStore which implements the IMXStore interface,
    allowing different backends (e.g. SQLite3) to be implemented at
    a later date.

---
+++ sdk/src/main/java/org/matrix/androidsdk/data/IMXStore.java
@@ -0,0 +1,12 @@
+package org.matrix.androidsdk.data;
+
+import org.matrix.androidsdk.api.response.User;
+
+/**
+ * An interface for storing and retrieving Matrix objects.
+ */
+public interface IMXStore {
+    public Room getRoom(String roomId);
+    public User getUser(String userId);
+    public void storeUser(User user);
+}
---
+++ sdk/src/main/java/org/matrix/androidsdk/data/MXMemoryStore.java
@@ -0,0 +1,54 @@
+package org.matrix.androidsdk.data;
+
+import org.matrix.androidsdk.api.response.User;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * An in-memory IMXStore.
+ */
+public class MXMemoryStore implements IMXStore {
+
+    private Map<String, Room> mRooms = new ConcurrentHashMap<String, Room>();
+    private Map<String, User> mUsers = new ConcurrentHashMap<String, User>();
+
+    public Collection<Room> getRooms() {
+        return mRooms.values();
+    }
+
+    public Collection<User> getUsers() {
+        return mUsers.values();
+    }
+
+    @Override
+    public Room getRoom(String roomId) {
+        return getOrCreateRoom(roomId);
+    }
+
+    @Override
+    public User getUser(String userId) {
+        return mUsers.get(userId);
+    }
+
+    @Override
+    public void storeUser(User user) {
+        mUsers.put(user.userId, user);
+    }
+
+    private Room addRoom(String roomId) {
+        Room room = new Room();
+        room.setRoomId(roomId);
+        mRooms.put(roomId, room);
+        return room;
+    }
+
+    private Room getOrCreateRoom(String roomId) {
+        Room room = mRooms.get(roomId);
+        if (room == null) {
+            room = addRoom(roomId);
+        }
+        return room;
+    }
+}
