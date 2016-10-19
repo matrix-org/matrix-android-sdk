22680266101bd5cc2ca4721cf55136163e0e8796
ylecollen <ylecollen@amdocs.com>
2016-10-19 16:51:09 +0200

    add_e2e_tests
    -> first dummy test

---
+++ matrix-sdk/src/test/java/org/matrix/androidsdk/crypto/CryptoTest.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2016 OpenMarket Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.matrix.androidsdk.crypto;
+
+import android.net.Uri;
+import android.os.Looper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.matrix.androidsdk.HomeserverConnectionConfig;
+import org.matrix.androidsdk.RestClient;
+import org.matrix.androidsdk.rest.callback.ApiCallback;
+import org.matrix.androidsdk.rest.client.LoginRestClient;
+import org.matrix.androidsdk.rest.client.MXRestExecutor;
+import org.matrix.androidsdk.rest.model.MatrixError;
+import org.matrix.androidsdk.rest.model.login.Credentials;
+import org.matrix.androidsdk.rest.model.login.LoginFlow;
+import org.matrix.androidsdk.util.MXOsHandler;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.internal.ShadowExtractor;
+import org.robolectric.shadows.ShadowLooper;
+import org.robolectric.util.Scheduler;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+// use RobolectricTestRunner else the java classes have to be mocked
+@RunWith(RobolectricTestRunner.class)
+public class CryptoTest {
+
+    /*
+     * Out of the box, the tests are supposed to be run with the iOS simulator attacking
+     * a test home server running on the same Mac machine.
+     * The reason is that the simulator can access to the home server running on the Mac
+     * via localhost. So everyone can use a localhost HS url that works everywhere.
+     * Here, we use one of the home servers launched by the  ./demo/start.sh --no-rate-limit script
+     */
+    private static final String MXTestsHomeServerURL = "http://localhost:8080";
+
+    private static final String MXTestsAliceDisplayName = "mxAlice";
+    private static final String MXTestsAliceAvatarURL = "mxc://matrix.org/kciiXusgZFKuNLIfLqmmttIQ";
+
+    private static final String MXTESTS_BOB  = "mxBob";
+    private static final String MXTESTS_BOB_PWD = "bobbob";
+
+    private static final String MXTESTS_ALICE = "mxAlice";
+    private static final String MXTESTS_ALICE_PWD ="alicealice";
+
+    private CountDownLatch mLock = new CountDownLatch(1);
+    private String password = null;
+
+    @Test
+    public void initTests() {
+        MXOsHandler.mPostListener = new MXOsHandler.IPostListener() {
+            @Override
+            public void onPost(Looper looper) {
+                ShadowLooper shadowLooper = (ShadowLooper)ShadowExtractor.extract(looper);
+
+                if (null != shadowLooper) {
+                    shadowLooper.idle();
+                }
+            }
+        };
+
+        RestClient.mcallbackExecutor = new MXRestExecutor();
+        RestClient.mHttpExecutor = new MXRestExecutor();
+    }
+
+    @Test
+    public void AccountsTest() throws Exception {
+
+        Uri uri = Uri.parse(MXTestsHomeServerURL);
+        HomeserverConnectionConfig hs = new HomeserverConnectionConfig(uri);
+
+        LoginRestClient loginRestClient = new LoginRestClient(hs);
+
+        loginRestClient.getSupportedLoginFlows(new ApiCallback<List<LoginFlow>>() {
+            @Override
+            public void onSuccess(List<LoginFlow> info) {
+                mLock.countDown();
+            }
+
+            @Override
+            public void onNetworkError(Exception e) {
+                mLock.countDown();
+            }
+
+            @Override
+            public void onMatrixError(MatrixError e) {
+                mLock.countDown();
+            }
+
+            @Override
+            public void onUnexpectedError(Exception e) {
+                mLock.countDown();
+            }
+        });
+
+        mLock.await(200000, TimeUnit.DAYS.MILLISECONDS);
+
+        loginRestClient.loginWithPassword(MXTESTS_BOB, "coucou", new ApiCallback<Credentials>() {
+            @Override
+            public void onSuccess(Credentials info) {
+                password = "aaa";
+                mLock.countDown();
+            }
+
+            @Override
+            public void onNetworkError(Exception e) {
+                mLock.countDown();
+            }
+
+            @Override
+            public void onMatrixError(MatrixError e) {
+                mLock.countDown();
+            }
+
+            @Override
+            public void onUnexpectedError(Exception e) {
+                mLock.countDown();
+            }
+        });
+
+        mLock.await(200000, TimeUnit.DAYS.MILLISECONDS);
+
+        assert (null != password);
+    }
+}
