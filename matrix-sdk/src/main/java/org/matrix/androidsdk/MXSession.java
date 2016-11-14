cda82f5bf5a877c7dc2b18c1e92576ed64b85139
pedroGitt <pedro.contreiras@amdocs.com>
2016-11-14 15:10:50 +0100

    Fix crash when getDirectChatRoomsDict() returns null

--- matrix-sdk/src/main/java/org/matrix/androidsdk/MXSession.java
+++ matrix-sdk/src/main/java/org/matrix/androidsdk/MXSession.java
@@ -1246,8 +1246,11 @@ public class MXSession {
     public List<String> getDirectChatRoomIdsList() {
         IMXStore store = getDataHandler().getStore();
         ArrayList<String> directChatRoomIdsList = new ArrayList<>();
+        Collection<List<String>> listOfList = null;
 
-        Collection<List<String>> listOfList = store.getDirectChatRoomsDict().values();
+        if (null != store.getDirectChatRoomsDict()) {
+            listOfList = store.getDirectChatRoomsDict().values();
+        }
 
         // if the direct messages entry has been defined
         if (null != listOfList) {
