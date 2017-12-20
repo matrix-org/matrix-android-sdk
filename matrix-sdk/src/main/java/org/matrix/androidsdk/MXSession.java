0cd4a37c5fb21542692d8133de2a8a7280744dd1
ylecollen <ylecollen@amdocs.com>
2017-12-20 16:08:47 +0100

    improve_media_cache Improve the media cache management
    1- the medias from encrypted rooms are stored encrypted
    2- the images are loaded asynchronously to avoid lags on main thread

--- matrix-sdk/src/main/java/org/matrix/androidsdk/MXSession.java
+++ matrix-sdk/src/main/java/org/matrix/androidsdk/MXSession.java
@@ -1056,6 +1056,10 @@ public class MXSession {
         } else {
             Log.e(LOG_TAG, "pauseEventStream : mEventsThread is null");
         }
+
+        if (null != getMediasCache()) {
+            getMediasCache().clearTmpCache();
+        }
     }
 
     /**
