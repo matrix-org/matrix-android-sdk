5daec26e373f8c91498df6dc524954c4c6506faa
Benoit Marty <benoit.marty@gmail.com>
2018-06-06 17:44:41 +0200

    Improve code quality check scripts (same as for Riot)

--- tools/check/check_code_quality.sh
+++ tools/check/check_code_quality.sh
@@ -16,10 +16,33 @@
 # limitations under the License.
 #
 
+#######################################################################################################################
+# Search forbidden pattern
+#######################################################################################################################
+
+searchForbiddenStringsScript=./tmp/search_forbidden_strings.pl
+
+if [ -f ${searchForbiddenStringsScript} ]; then
+  echo "${searchForbiddenStringsScript} already there"
+else
+  mkdir tmp
+  echo "Get the script"
+  wget https://raw.githubusercontent.com/matrix-org/matrix-dev-tools/develop/bin/search_forbidden_strings.pl -O ${searchForbiddenStringsScript}
+fi
+
+echo
+
+if [ -x ${searchForbiddenStringsScript} ]; then
+  echo "${searchForbiddenStringsScript} is already executable"
+else
+  echo "Make the script executable"
+  chmod u+x ${searchForbiddenStringsScript}
+fi
+
 echo
 echo "Search for forbidden patterns in code..."
 
-./tools/check/search_forbidden_strings.pl ./tools/check/forbidden_strings_in_code.txt \
+${searchForbiddenStringsScript} ./tools/check/forbidden_strings_in_code.txt \
     ./matrix-sdk/src/main/java \
     ./matrix-sdk/src/test/java
 
@@ -28,15 +51,49 @@ resultForbiddenStringInCode=$?
 echo
 echo "Search for forbidden patterns in resources..."
 
-./tools/check/search_forbidden_strings.pl ./tools/check/forbidden_strings_in_resources.txt \
+${searchForbiddenStringsScript} ./tools/check/forbidden_strings_in_resources.txt \
     ./matrix-sdk/src/main/res/layout \
     ./matrix-sdk/src/main/res/values
 
 resultForbiddenStringInResource=$?
 
+#######################################################################################################################
+# Check files with long lines
+#######################################################################################################################
+
+checkLongFilesScript=./tmp/check_long_files.pl
+
+if [ -f ${checkLongFilesScript} ]; then
+  echo "${checkLongFilesScript} already there"
+else
+  mkdir tmp
+  echo "Get the script"
+  wget https://raw.githubusercontent.com/matrix-org/matrix-dev-tools/develop/bin/check_long_files.pl -O ${checkLongFilesScript}
+fi
+
 echo
 
-if [ $resultForbiddenStringInCode -eq 0 ] && [ $resultForbiddenStringInResource -eq 0 ]; then
+if [ -x ${checkLongFilesScript} ]; then
+  echo "${checkLongFilesScript} is already executable"
+else
+  echo "Make the script executable"
+  chmod u+x ${checkLongFilesScript}
+fi
+
+echo
+echo "Search for long files..."
+
+${checkLongFilesScript} 3000 \
+    ./matrix-sdk/src/main/java \
+    ./matrix-sdk/src/test/java \
+    ./matrix-sdk/src/main/res/layout \
+    ./matrix-sdk/src/main/res/values
+
+resultLongFiles=$?
+
+echo
+
+if [ ${resultForbiddenStringInCode} -eq 0 ] && [ ${resultForbiddenStringInResource} -eq 0 ] && [ ${resultLongFiles} -eq 0 ]; then
    echo "MAIN OK"
 else
    echo "MAIN ERROR"
