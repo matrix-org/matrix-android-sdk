e5a30a81fdd15ad67f9bb01c796448edce62faee
Benoit Marty <benoit.marty@gmail.com>
2018-06-26 16:22:36 +0200

    Increase timeout again to be sure all the tests are successful

--- matrix-sdk/src/androidTest/java/org/matrix/androidsdk/TestConstants.java
+++ matrix-sdk/src/androidTest/java/org/matrix/androidsdk/TestConstants.java
@@ -18,5 +18,5 @@ package org.matrix.androidsdk;
 
 class TestConstants {
     // Time out to use when waiting for server response
-    public static final int AWAIT_TIME_OUT_MILLIS = 10000;
+    public static final int AWAIT_TIME_OUT_MILLIS = 30000;
 }
