8735c169dd0721c8bedc8d592e357c2989a2635b
Benoit Marty <benoit.marty@gmail.com>
2018-08-23 09:48:47 +0200

    Stack callbacks to avoid requested several times the room members

--- matrix-sdk/src/main/java/org/matrix/androidsdk/data/RoomState.java
+++ matrix-sdk/src/main/java/org/matrix/androidsdk/data/RoomState.java
@@ -168,6 +168,8 @@ public class RoomState implements Externalizable {
     // true if all members are loaded
     private boolean mAllMembersAreLoaded;
 
+    private List<ApiCallback<List<RoomMember>>> mGetAllMembersCallbacks = new ArrayList<>();
+
     // the third party invite members
     private final Map<String, RoomThirdPartyInvite> mThirdPartyInvites = new HashMap<>();
 
@@ -264,7 +266,7 @@ public class RoomState implements Externalizable {
     /**
      * Get the list of all the room members. Fetch from server if the full list is not loaded yet.
      */
-    public void getMembersAsync(final ApiCallback<List<RoomMember>> callback) {
+    public void getMembersAsync(ApiCallback<List<RoomMember>> callback) {
         if (mAllMembersAreLoaded) {
             List<RoomMember> res;
 
@@ -275,26 +277,42 @@ public class RoomState implements Externalizable {
 
             callback.onSuccess(res);
         } else {
-            // Load members from server
-            getDataHandler().getMembersAsync(roomId, new SimpleApiCallback<List<RoomMember>>(callback) {
-                @Override
-                public void onSuccess(List<RoomMember> info) {
-                    List<RoomMember> res;
+            boolean doTheRequest;
 
-                    synchronized (this) {
+            synchronized (mGetAllMembersCallbacks) {
+                mGetAllMembersCallbacks.add(callback);
+
+                doTheRequest = mGetAllMembersCallbacks.size() == 1;
+            }
+
+            if (doTheRequest) {
+                // Load members from server
+                getDataHandler().getMembersAsync(roomId, new SimpleApiCallback<List<RoomMember>>(callback) {
+                    @Override
+                    public void onSuccess(List<RoomMember> info) {
+                        List<RoomMember> res;
+
                         for (RoomMember member : info) {
                             setMember(member.getUserId(), member);
                         }
 
-                        // make a copy to avoid concurrency modifications
-                        res = new ArrayList<>(mMembers.values());
+                        synchronized (mGetAllMembersCallbacks) {
+                            for (ApiCallback<List<RoomMember>> apiCallback : mGetAllMembersCallbacks) {
+                                synchronized (this) {
+                                    // make a copy to avoid concurrency modifications
+                                    res = new ArrayList<>(mMembers.values());
+                                }
+
+                                apiCallback.onSuccess(res);
+                            }
+
+                            mGetAllMembersCallbacks.clear();
+                        }
 
                         mAllMembersAreLoaded = true;
                     }
-
-                    callback.onSuccess(res);
-                }
-            });
+                });
+            }
         }
     }
 
@@ -375,7 +393,7 @@ public class RoomState implements Externalizable {
      * Provides a list of displayable members.
      * Some dummy members are created to internal stuff.
      *
-     * @return a copy of the displayable room members list.
+     * @param callback The callback to get a copy of the displayable room members list.
      */
     public void getDisplayableMembers(final ApiCallback<List<RoomMember>> callback) {
         getMembersAsync(new SimpleApiCallback<List<RoomMember>>(callback) {
@@ -392,7 +410,6 @@ public class RoomState implements Externalizable {
                 }
             }
         });
-
     }
 
     /**
