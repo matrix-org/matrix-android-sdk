eb3b8c7214d67b6599a0aeae3bd0dd9d2e45fcd8
Benoit Marty <benoit.marty@gmail.com>
2018-09-14 14:37:10 +0200

    Add a method from riot to extract id from a Matrix permalink

--- matrix-sdk/src/main/java/org/matrix/androidsdk/util/PermalinkUtils.java
+++ matrix-sdk/src/main/java/org/matrix/androidsdk/util/PermalinkUtils.java
@@ -23,7 +23,7 @@ import android.text.TextUtils;
 import org.matrix.androidsdk.rest.model.Event;
 
 /**
- * Useful methods to create permalink
+ * Useful methods to deals with Matrix permalink
  */
 public class PermalinkUtils {
 
@@ -31,7 +31,7 @@ public class PermalinkUtils {
 
     /**
      * Creates a permalink for an event.
-     * Ex: https://matrix.to/#/!nbzmcXAqpxBXjAdgoX:matrix.org/$1531497316352799BevdV:matrix.org
+     * Ex: "https://matrix.to/#/!nbzmcXAqpxBXjAdgoX:matrix.org/$1531497316352799BevdV:matrix.org"
      *
      * @param event the event
      * @return the permalink, or null in case of error
@@ -47,7 +47,7 @@ public class PermalinkUtils {
 
     /**
      * Creates a permalink for an id (can be a user Id, Room Id, etc.).
-     * Ex: https://matrix.to/#/@benoit:matrix.org
+     * Ex: "https://matrix.to/#/@benoit:matrix.org"
      *
      * @param id the id
      * @return the permalink, or null in case of error
@@ -63,16 +63,33 @@ public class PermalinkUtils {
 
     /**
      * Creates a permalink for an event. If you have an event you can use {@link #createPermalink(Event)}
-     * Ex: https://matrix.to/#/!nbzmcXAqpxBXjAdgoX:matrix.org/$1531497316352799BevdV:matrix.org
+     * Ex: "https://matrix.to/#/!nbzmcXAqpxBXjAdgoX:matrix.org/$1531497316352799BevdV:matrix.org"
      *
      * @param roomId  the id of the room
      * @param eventId the id of the event
      * @return the permalink
      */
-    @Nullable
+    @NonNull
     public static String createPermalink(@NonNull String roomId, @NonNull String eventId) {
         return MATRIX_TO_URL_BASE + escape(roomId) + "/" + escape(eventId);
     }
+
+    /**
+     * Extract the linked id from the universal link
+     *
+     * @param url the universal link, Ex: "https://matrix.to/#/@benoit:matrix.org"
+     * @return the id from the url, ex: "@benoit:matrix.org", or null if the url is not a permalink
+     */
+    public static String getLinkedId(@Nullable String url) {
+        boolean isSupported = url != null && url.startsWith(MATRIX_TO_URL_BASE);
+
+        if (isSupported) {
+            return url.substring(MATRIX_TO_URL_BASE.length());
+        }
+
+        return null;
+    }
+
 
     /**
      * Escape '/' in id, because it is used as a separator
