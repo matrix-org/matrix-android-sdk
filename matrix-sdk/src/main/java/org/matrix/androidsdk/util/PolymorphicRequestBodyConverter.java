e23c638b7f950b02b013aca91875c0cc74ec0b9d
Benoit Marty <benoit@matrix.org>
2018-11-30 15:10:28 +0100

    Add copyright and format

--- matrix-sdk/src/main/java/org/matrix/androidsdk/util/PolymorphicRequestBodyConverter.java
+++ matrix-sdk/src/main/java/org/matrix/androidsdk/util/PolymorphicRequestBodyConverter.java
@@ -1,5 +1,20 @@
-package org.matrix.androidsdk.util;
+/*
+ * Copyright 2018 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package org.matrix.androidsdk.util;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
@@ -13,14 +28,15 @@ import retrofit2.Retrofit;
 
 public final class PolymorphicRequestBodyConverter<T> implements Converter<T, RequestBody> {
     public static final Factory FACTORY = new Factory() {
-        @Override public Converter<?, RequestBody> requestBodyConverter(
-            Type type,
-            Annotation[] parameterAnnotations,
-            Annotation[] methodAnnotations,
-            Retrofit retrofit
+        @Override
+        public Converter<?, RequestBody> requestBodyConverter(
+                Type type,
+                Annotation[] parameterAnnotations,
+                Annotation[] methodAnnotations,
+                Retrofit retrofit
         ) {
             return new PolymorphicRequestBodyConverter<>(
-                this, parameterAnnotations, methodAnnotations, retrofit
+                    this, parameterAnnotations, methodAnnotations, retrofit
             );
         }
     };
@@ -32,17 +48,18 @@ public final class PolymorphicRequestBodyConverter<T> implements Converter<T, Re
     private final Map<Class<?>, Converter<T, RequestBody>> cache = new LinkedHashMap<>();
 
     PolymorphicRequestBodyConverter(
-        Factory skipPast,
-        Annotation[] parameterAnnotations,
-        Annotation[] methodsAnnotations,
-        Retrofit retrofit) {
+            Factory skipPast,
+            Annotation[] parameterAnnotations,
+            Annotation[] methodsAnnotations,
+            Retrofit retrofit) {
         this.skipPast = skipPast;
         this.parameterAnnotations = parameterAnnotations;
         this.methodsAnnotations = methodsAnnotations;
         this.retrofit = retrofit;
     }
 
-    @Override public RequestBody convert(T value) throws IOException {
+    @Override
+    public RequestBody convert(T value) throws IOException {
         Class<?> cls = value.getClass();
         Converter<T, RequestBody> requestBodyConverter;
         synchronized (cache) {
@@ -50,7 +67,7 @@ public final class PolymorphicRequestBodyConverter<T> implements Converter<T, Re
         }
         if (requestBodyConverter == null) {
             requestBodyConverter = retrofit.nextRequestBodyConverter(
-                skipPast, cls, parameterAnnotations, methodsAnnotations
+                    skipPast, cls, parameterAnnotations, methodsAnnotations
             );
             synchronized (cache) {
                 cache.put(cls, requestBodyConverter);
