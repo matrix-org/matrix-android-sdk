a27b3653d05de928e3d021d74e75d4391e62a782
Valere <valeref@matrix.org>
2019-10-08 17:55:06 +0200

    Defensive coding against malformed IS

--- matrix-sdk/src/main/java/org/matrix/androidsdk/features/identityserver/IdentityServerManager.kt
+++ matrix-sdk/src/main/java/org/matrix/androidsdk/features/identityserver/IdentityServerManager.kt
@@ -270,12 +270,18 @@ class IdentityServerManager(val mxSession: MXSession,
             identityAuthRestClient = null
             thirdPidRestClient = null
         } else {
-            val alteredHsConfig = HomeServerConnectionConfig.Builder(mxSession.homeServerConfig)
-                    .withIdentityServerUri(Uri.parse(newUrl))
-                    .build()
+            try {
+                val alteredHsConfig = HomeServerConnectionConfig.Builder(mxSession.homeServerConfig)
+                        .withIdentityServerUri(Uri.parse(newUrl))
+                        .build()
 
-            identityAuthRestClient = IdentityAuthRestClient(alteredHsConfig)
-            thirdPidRestClient = ThirdPidRestClient(alteredHsConfig)
+                identityAuthRestClient = IdentityAuthRestClient(alteredHsConfig)
+                thirdPidRestClient = ThirdPidRestClient(alteredHsConfig)
+            } catch (t: Throwable) {
+                Log.e(LOG_TAG, "Failed to create IS Rest clients", t)
+                //What to do from there? this IS is invalid
+                return
+            }
         }
 
         synchronized(listeners) { listeners.forEach { it.onIdentityServerChange() } }
@@ -302,6 +308,11 @@ class IdentityServerManager(val mxSession: MXSession,
     }
 
     private fun requestIdentityServerToken(requestOpenIdTokenResponse: RequestOpenIdTokenResponse, callback: ApiCallback<String>) {
+        if (identityAuthRestClient == null) {
+            callback.onUnexpectedError(IdentityServerNotConfiguredException())
+            return
+        }
+
         identityAuthRestClient?.register(requestOpenIdTokenResponse, object : SimpleApiCallback<IdentityServerRegisterResponse>(callback) {
             override fun onSuccess(info: IdentityServerRegisterResponse) {
                 // Store the token for next time
@@ -596,7 +607,7 @@ class IdentityServerManager(val mxSession: MXSession,
                 override fun onUnexpectedError(e: Exception) {
                     if (e is IdentityServerV2ApiNotAvailable) {
                         //mm ? request to HS?
-                        legacyDeleteAndRequestToken(threePid,callback,idServer)
+                        legacyDeleteAndRequestToken(threePid, callback, idServer)
                     } else {
                         super.onUnexpectedError(e)
                     }
@@ -659,7 +670,7 @@ class IdentityServerManager(val mxSession: MXSession,
                 override fun onUnexpectedError(e: Exception) {
                     if (e is IdentityServerV2ApiNotAvailable) {
                         //mm ? request to HS?
-                        legacyDeleteAndAddMsisdn(threePid,callback,idServer)
+                        legacyDeleteAndAddMsisdn(threePid, callback, idServer)
                     } else {
                         super.onUnexpectedError(e)
                     }
