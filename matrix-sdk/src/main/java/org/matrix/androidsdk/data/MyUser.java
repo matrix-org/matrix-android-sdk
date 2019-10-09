a5f099f73ebd1ebc60e8dcfd7af28ea95b3a60a6
Valere <valeref@matrix.org>
2019-10-09 14:25:40 +0200

    code review fix

--- matrix-sdk/src/main/java/org/matrix/androidsdk/data/MyUser.java
+++ matrix-sdk/src/main/java/org/matrix/androidsdk/data/MyUser.java
@@ -126,9 +126,9 @@ public class MyUser extends User {
     private void buildIdentifiersLists() {
         mEmailIdentifiers = new ArrayList<>();
         mPhoneNumberIdentifiers = new ArrayList<>();
-        List<ThirdPartyIdentifier> identifiers = mDataHandler.getStore().thirdPartyIdentifiers();
         //NPE reported on playstore
         if (mDataHandler.getStore() != null) {
+            List<ThirdPartyIdentifier> identifiers = mDataHandler.getStore().thirdPartyIdentifiers();
             for (ThirdPartyIdentifier identifier : identifiers) {
                 switch (identifier.medium) {
                     case ThreePid.MEDIUM_EMAIL:
